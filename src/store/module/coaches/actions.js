f274999eae07cafbbe258d859f2878438222a1d9
mygit <purceladzedavit@gmail.com>
2023-03-01 04:42:10 +0400

    attach token to outgoing requests

--- src/store/module/coaches/actions.js
+++ src/store/module/coaches/actions.js
@@ -9,9 +9,11 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas,
     };
+    const token = context.rootGetters.token;
 
     const response = await fetch(
-      `https://find-a-coach-241a1-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://find-a-coach-241a1-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
